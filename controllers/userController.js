e43696befc9084434d11110965b6a0068c00585d
nettour <nettour.dz@gmail.com>
2025-04-09 19:13:56 +0300

    add validator

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,3 +17,18 @@ exports.userCreatePost = (req, res)=>{
     res.redirect('/')   
 }
 
+//validation
+const {body, validationResult} = require('express-validator')
+
+const alphaErr = 'must only contain letter'
+const lengthErr = 'must be between 0 and 10 charachters'
+
+const validateUser = [
+    body('firstName').trim()
+        .isAlpha().withMessage(`First Name ${alphaErr}`)
+        .isLength({min:1, max:10}).withMessage(`First Name ${lengthErr}`),
+
+    body('lastName').trim()
+        .isAlpha().withMessage(`Last Name ${alphaErr}`)
+        .isLength({min:1, max:10}).withMessage(`Last Name ${lengthErr}`)
+]
